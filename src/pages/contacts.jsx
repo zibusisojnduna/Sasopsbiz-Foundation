640b123fc3be4908183c3b382f6336de0ce2c75a
Zibusiso Nduna <you@example.com>
2025-08-26 15:04:44 +0200

    react icons added

--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -1,3 +1,6 @@
+import { MdOutlineEmail } from "react-icons/md";
+import { FaPhone } from "react-icons/fa6";
+import { IoMdPin } from "react-icons/io";
 function Contacts(){
     return(
         <>
@@ -16,8 +19,20 @@ function Contacts(){
             </div>
 
             <div style={{margin:"5%", textAlign:"center", padding:"5%", display:""}}>
-                
+                <MdOutlineEmail style={{color:"orange", size:"20"}} /><h3>Email Address:</h3><br></br>
+                <h4>siphelele@sasopsbiz</h4>
             </div>
+
+            <div style={{margin:"5%", textAlign:"center", padding:"5%", display:""}}>
+                <FaPhone style={{color:"orange"}} /><h3>Phone Number:</h3><br></br>
+                <h4>033 307 0898</h4>
+            </div>
+
+            <div style={{margin:"5%", textAlign:"center", padding:"5%", display:""}}>
+                <IoMdPin style={{color:"orange"}} /><h3>Physical Address:</h3>
+                <h4>210 Longmarket Steet, Pietermaritzburg, 3201</h4>
+            </div>
+
         </section>
             
         </>
