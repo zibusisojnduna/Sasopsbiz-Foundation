2128178a52fc5380373fe9d275e035548f7cf225
Zibusiso Nduna <you@example.com>
2025-10-29 14:24:37 +0200

    redirects added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '' // copy to dist root
+        }
+      ]
+    })],
 })
