7b0b94a836419b294d5d8cf5e24190a624d817e7
Zibusiso Nduna <you@example.com>
2025-10-29 14:54:06 +0200

    redirects removed, static.json added

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), viteStaticCopy({
-      targets: [
-        {
-          src: 'public/_redirects',
-          dest: '' // copy to dist root
-        }
-      ]
-    })],
+  plugins: [react()],
 })
